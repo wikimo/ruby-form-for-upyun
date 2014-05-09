a4f0860cb296a442181d43a090a204e33acf3ea8
wikimo <gwikimo@gmail.com>
2014-05-09 13:11:40 +0800

    update code format

--- spec/upyun_spec.rb
+++ spec/upyun_spec.rb
@@ -2,41 +2,41 @@ require File.dirname(__FILE__) + '/spec_helper'
 require 'open-uri'
 
 describe 'Upyun' do
-	before :all do
-		@config = {:bucket => 'devel',
-					:secret => 'lRJmqjI19GxB80KJc7y7NOcI+8g='
-				}
+  before :all do
+    @config = {:bucket => 'devel',
+	    :secret => 'lRJmqjI19GxB80KJc7y7NOcI+8g='
+    }
 
     
-	end	
+  end	
 
-	it 'put file without return url' do
+  it 'put file without return url' do
 
-		@uploader = Upyun::Form::Uploader.new @config
+    @uploader = Upyun::Form::Uploader.new @config
 
-		json = @uploader.put './test.jpeg'
+    json = @uploader.put './test.jpeg'
 		
-		expect(json[:code]).to eq  200
-	end	
+    expect(json[:code]).to eq  200
+  end	
 
-	it 'put file with return url' do
-		@config[:return_url] = 'http://localhost'
+  it 'put file with return url' do
+    @config[:return_url] = 'http://localhost'
 
-		@uploader = Upyun::Form::Uploader.new @config
+    @uploader = Upyun::Form::Uploader.new @config
 
-		response = @uploader.put './test.jpeg'
-		expect(response['location']).to include @config[:return_url]
-	end	
+    response = @uploader.put './test.jpeg'
+    expect(response['location']).to include @config[:return_url]
+  end	
 
-	it 'put file with notify url' do
-		@config[:notify_url] = 'http://dev.365jinbi.com/notify.php'
+  it 'put file with notify url' do
+    @config[:notify_url] = 'http://dev.365jinbi.com/notify.php'
 
-		@uploader = Upyun::Form::Uploader.new @config
-		json = @uploader.put './test.jpeg'
+    @uploader = Upyun::Form::Uploader.new @config
+    json = @uploader.put './test.jpeg'
 
-		sleep 5
+    sleep 5
 
-		notify_info  = open('http://dev.365jinbi.com/upyun.log').read
-		expect(notify_info).to include json[:url]
-	end	
+    notify_info  = open('http://dev.365jinbi.com/upyun.log').read
+    expect(notify_info).to include json[:url]
+  end	
 end
