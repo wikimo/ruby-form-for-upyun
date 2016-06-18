5bb588b64f2a10e9f57989eed710ac22fc03e369
sunchi <sunchi1119@gmail.com>
2016-06-18 16:07:21 +0800

    add app

--- lib/upyun/form.rb
+++ lib/upyun/form.rb
@@ -34,12 +34,24 @@ module Upyun
        opt = {}
        opt['bucket'] =  @bucket
        save_path =  @config[:save_path] ? @config[:save_path] : Time.now.strftime("%Y-%m-%d")
-       opt['save-key'] =  "/#{save_path}/#{SecureRandom.uuid}.#{mime_type.sub_type}"
+       filename = SecureRandom.uuid
+
+       # opt['save-key'] =  "/#{save_path}/#{SecureRandom.uuid}.#{mime_type.sub_type}"
+       opt['save-key'] =  "/#{save_path}/#{filename}.jpg"
        opt['expiration'] = Time.now.to_i + 600
 
        opt['return-url'] = @config[:return_url] if @config[:return_url] != ''
        opt['notify-url'] = @config[:notify_url] if @config[:notify_url] != ''
+       opt['content-secret'] = @config[:content_secret] if @config[:content_secret] != ''
 
+        if @config[:apps] != ''
+          @config[:apps].each_with_index do |app, index|
+            app[:save_as] = "/#{save_path}/#{filename}#{app['save_tag']}.jpg"
+            @config[:apps][index] = app
+          end
+          opt['apps'] = @config[:apps]
+        end
+        
        policy = Base64.encode64(opt.to_json).gsub(/\n/,'')
        sign =  Digest::MD5.hexdigest("#{policy}&#{@form_api_secret}")
 
